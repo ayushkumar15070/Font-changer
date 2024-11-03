9fdc8fee10fce3eace090f132f6c9038e3be41a3
Ayush Kumar Vishwakarma <ayushkv0403@gmail.com>
2024-11-03 13:04:40 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,11 @@
+let poppins = document.getElementById("poppins")
+let itim = document.getElementById("itim")
+let text = document.getElementById("text")
+
+poppins.addEventListener("click", function () {
+      text.style.fontFamily = "Poppins";
+})
+
+itim.addEventListener("click", function () {
+      text.style.fontFamily = "Itim";
+})
